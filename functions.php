262af22d542d5d4f53fb3d14c75dffcb96d488f2
Divya21146 <113895154+Divya21146@users.noreply.github.com>
2023-12-08 17:57:09 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function add_column_to_custom_table() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . "tablename";  //replace with your table name
+
+    $wpdb->query("ALTER TABLE $table_name ADD COLUMN new_column_name VARCHAR(255) NOT NULL AFTER existing_column_name;"); // Replace "new_column_name" with the desired name for the new column and specify the location by providing the name of the existing column in "existing_column_name."
+}
+
+add_action('init', 'add_column_to_custom_table');
